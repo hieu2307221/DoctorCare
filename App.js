659666e8edfb0f013d4fe4cfc03ec2e2f5f4d2d5
hieutrandev <hieungocdev@gmail.com>
2019-01-12 01:02:41 +0700

    Khoi tao du an

---
+++ App.js
@@ -0,0 +1,13 @@
+
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Home } from './src/Route';
+export default class App extends Component{
+  render() {
+    return (
+      < Home />
+    );
+  }
+}
+
---
+++ src/Route.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {StackNavigator} from 'react-navigation';
+import Intro from './Components/Intro/Intro';
+export const Home = StackNavigator({
+    main:{
+        screen: Intro
+    }
+});
